f565e10928286cb8d027b1dff236e9cf98c55027
everstudybee <everstudybee@gmail.com>
2022-09-30 11:44:30 +0200

    initial commit

---
+++ Database/Data/TestProjectContext.cs
@@ -0,0 +1,15 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Data
+{
+    public class TestProjectContext : DbContext
+    {
+        public TestProjectContext(DbContextOptions<TestProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Name> Names { get; set; } = null!;
+    }
+}
---
+++ Database/Models/Name.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Database.Models
+{
+    public class Name
+    {
+        [Key]
+        public int NameId { get; set; }
+
+        public string? PersonName { get; set; }
+    }
+}
